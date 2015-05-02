98324af6328f612ddcbc910397ebf2ebc419c5a9
Samuel Tardieu <sam@rfc1149.net>
2015-05-02 21:55:03 +0200

    Use extends AnyVal to prevent extraneous instantiations

--- src/main/scala/net/rfc1149/canape/implicits.scala
+++ src/main/scala/net/rfc1149/canape/implicits.scala
@@ -7,7 +7,7 @@ import scala.language.implicitConversions
 object implicits {
 
   // TODO: remove after the transition phase
-  implicit class CouchRequestEmulation[T](val f: Future[T]) {
+  implicit class CouchRequestEmulation[T](val f: Future[T]) extends AnyVal {
     @deprecated("data is already a future", "spray") def toFuture(): Future[T] = f
     def execute()(implicit timeout: Duration): T = Await.result(f, timeout)
   }
